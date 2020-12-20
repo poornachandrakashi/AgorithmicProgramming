d3151eb441fcacdc088fc945b02fe802177bb6d2
poornachandra kashi <44542685+poornachandrakashi@users.noreply.github.com>
2020-12-20 11:15:58 +0530

    Updated with Stack Link

--- stackLink.cpp
+++ stackLink.cpp
@@ -1,9 +1,9 @@
-#inlcude < iostream>
+#include <iostream>
 
 struct node
 {
-    int val,
-        node *next;
+    int val;
+    node *next;
 };
 
 node *top_var;
@@ -18,7 +18,7 @@ void push(int x)
 
 void pop()
 {
-    if (top_var == Null)
+    if (top_var == NULL)
     {
         std::cout << "\nUnderflow";
     }
@@ -26,7 +26,7 @@ void pop()
     {
         node *t = top_var;
         std::cout << "\n"
-                  << t->val << "deleted";
+                  << t->val << " deleted";
         top_var = top_var->next;
         delete t;
     }
@@ -34,4 +34,39 @@ void pop()
 
 void show()
 {
+    node *t = top_var;
+    while (t != NULL)
+    {
+        std::cout << t->val << "\n";
+        t = t->next;
+    }
+}
+
+int main()
+{
+    int ch, x;
+    do
+    {
+        std::cout << "\n1. Push";
+        std::cout << "\n2. Pop";
+        std::cout << "\n3. Print";
+        std::cout << "\nEnter Your Choice : ";
+        std::cin >> ch;
+        if (ch == 1)
+        {
+            std::cout << "\nInsert : ";
+            std::cin >> x;
+            push(x);
+        }
+        else if (ch == 2)
+        {
+            pop();
+        }
+        else if (ch == 3)
+        {
+            show();
+        }
+    } while (ch != 0);
+
+    return 0;
 }
